270d93cd3bad97f5114045154036e1a84cd8b9b1
hardikkumar.hadwani <hardikkumar.hadwani@hurix.com>
2023-03-04 01:41:18 +0530

    Adding checksum

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
          // Dependencies declare other packages that this package depends on.
          // .package(url: /* package url */, from: "1.0.0"),
     ],
-    targets: [
-        .binaryTarget(name: "Kitaboo_Reader_SDK", url: "https://github.com/hardikhadwani-hurix/KitabooReaderSDK/releases/download/1.0/Kitaboo_Reader_SDK.xcframework.zip", checksum: "daf7070fd56eb33815835ec632823b31e5144f665090d53b8a613dbe76ee2911"),
+    targets: [ 
+        .binaryTarget(name: "KitabooBookShelfFramework", url: "https://github.com/hardikhadwani-hurix/KitabooBookShelfSPM/releases/download/1.0.0/KitabooBookShelfFramework.xcframework.zip", checksum: "72ed0b7176d41365cf40f07edda1b82320e871378e08bb45375f9525ada03eab"),
 
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.

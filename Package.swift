60d523a755a8b979edcb52e01b6dbd9303d560fb
hardikkumar.hadwani <hardikkumar.hadwani@hurix.com>
2023-03-23 14:36:40 +0530

    Resolved issue related 'sharedApplication' is unavailable: not available on iOS (App Extension)

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
          // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [ 
-        .binaryTarget(name: "KitabooBookShelfFramework", url: "https://github.com/hardikhadwani-hurix/KitabooBookShelfSPM/releases/download/1.0.8/KitabooBookShelfFramework.xcframework.zip", checksum: "5d68cbec8c132b78d9fcd4d8545c46e0a2586d4992bd3f0b8374c22061c65e8e"),
+        .binaryTarget(name: "KitabooBookShelfFramework", url: "https://github.com/hardikhadwani-hurix/KitabooBookShelfSPM/releases/download/1.0.11/KitabooBookShelfFramework.xcframework.zip", checksum: "5d68cbec8c132b78d9fcd4d8545c46e0a2586d4992bd3f0b8374c22061c65e8e"),
 
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.

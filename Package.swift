cc7b347aa5809e9a0105ec9a5654e1071e8d0f57
hardikkumar.hadwani <hardikkumar.hadwani@hurix.com>
2023-03-23 15:11:15 +0530

    Resolved issue related 'sharedApplication' is unavailable: not available on iOS (App Extension)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["KitabooBookShelfSPM","KitabooBookShelfFramework"]),
     ],
     dependencies: [
-         .package(url: "https://github.com/hardikhadwani-hurix/KitabooSDKWithReader", from: "1.0.6"),
+         .package(url: "https://github.com/hardikhadwani-hurix/KitabooSDKWithReader", from: "1.0.7"),
          // Dependencies declare other packages that this package depends on.
          // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -30,3 +30,4 @@ let package = Package(
             dependencies: ["KitabooBookShelfSPM"]),
     ]
 )
+

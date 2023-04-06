05d86c8627197a719c72612276a1844d11208b31
hardikkumar.hadwani <hardikkumar.hadwani@hurix.com>
2023-04-06 17:50:19 +0530

    Resolved crash issue

--- Package.swift
+++ Package.swift
@@ -13,12 +13,12 @@ let package = Package(
             targets: ["KitabooBookShelfSPM","KitabooBookShelfFramework"]),
     ],
     dependencies: [
-         .package(url: "https://github.com/hardikhadwani-hurix/KitabooSDKWithReader", from: "1.0.7"),
+         .package(url: "https://github.com/hardikhadwani-hurix/KitabooSDKWithReader", from: "1.0.8"),
          // Dependencies declare other packages that this package depends on.
          // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [ 
-        .binaryTarget(name: "KitabooBookShelfFramework", url: "https://github.com/hardikhadwani-hurix/KitabooBookShelfSPM/releases/download/1.0.11/KitabooBookShelfFramework.xcframework.zip", checksum: "5d68cbec8c132b78d9fcd4d8545c46e0a2586d4992bd3f0b8374c22061c65e8e"),
+        .binaryTarget(name: "KitabooBookShelfFramework", url: "https://github.com/hardikhadwani-hurix/KitabooBookShelfSPM/releases/download/1.0.12/KitabooBookShelfFramework.xcframework.zip", checksum: "99fcbef9701dd1ace068b9c67847d748d56c86eba9ef65630259a7fd1bf57c30"),
 
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.

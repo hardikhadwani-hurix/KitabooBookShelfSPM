10974159d9b5889251d394cb417966e2558c372f
hardikkumar.hadwani <hardikkumar.hadwani@hurix.com>
2023-04-07 17:22:20 +0530

    Resolved help screen issue

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["KitabooBookShelfSPM","KitabooBookShelfFramework"]),
     ],
     dependencies: [
-         .package(url: "https://github.com/hardikhadwani-hurix/KitabooSDKWithReader", from: "1.0.10"),
+         .package(url: "https://github.com/hardikhadwani-hurix/KitabooSDKWithReader", from: "1.0.11"),
          // Dependencies declare other packages that this package depends on.
          // .package(url: /* package url */, from: "1.0.0"),
     ],

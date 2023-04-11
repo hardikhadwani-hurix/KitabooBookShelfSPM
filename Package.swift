ab1f8f789b3e7bd71179b2fd2ee2e2f1b372079b
hardikkumar.hadwani <hardikkumar.hadwani@hurix.com>
2023-04-11 13:06:27 +0530

    category slider issue

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
          // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [ 
-        .binaryTarget(name: "KitabooBookShelfFramework", url: "https://github.com/hardikhadwani-hurix/KitabooBookShelfSPM/releases/download/1.0.13/KitabooBookShelfFramework.xcframework.zip", checksum: "8937af4c14478148916fd21721380048ae5e869f0e2a046b09d60d8d0a44ee10"),
+        .binaryTarget(name: "KitabooBookShelfFramework", url: "https://github.com/hardikhadwani-hurix/KitabooBookShelfSPM/releases/download/1.0.16/KitabooBookShelfFramework.xcframework.zip", checksum: "8937af4c14478148916fd21721380048ae5e869f0e2a046b09d60d8d0a44ee10"),
 
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
